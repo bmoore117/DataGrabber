5c7b587a2123f0d8259a48541adefb5e52479c91
Benjamin Moore <american.exceptionalist@gmail.com>
2017-05-15 20:26:53 -0400

    Switch to bittrex

--- datagrabber.js
+++ datagrabber.js
@@ -1,36 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var autobahn_1 = require("autobahn");
 var mongodb_1 = require("mongodb");
+var url = require("url");
+var https = require("https");
 var dbConn = mongodb_1.MongoClient.connect("mongodb://localhost:27017/db", function (error, db) {
     if (error) {
-        console.log("Error object truthy");
-        return console.dir(error);
+        console.error(error);
+        return;
     }
-    var lastBid = [];
-    var lastAsk = [];
-    var lastTrade = [];
-    db.createCollection('marketdata', function (error, collection) {
-        var exchangeConn = new autobahn_1.Connection({ url: 'wss://api.poloniex.com', realm: 'realm1' });
-        exchangeConn.onopen = function (session) {
-            function marketEvent(args, kwargs) {
-                //have var for last trade, best bid, and best ask
-                //on receipt of new non empty data
-                //find best bid & best ask, and last trade
-                //compare to previous & update
-                //join and store last trade, best bid, best ask
-                if (args.length > 0) {
-                    //parse here
-                    console.log(args);
-                    console.log(kwargs);
-                    //collection.insert(args);
-                    //console.log("Record stored");
-                }
-            }
-            session.subscribe('BTC_XMR', marketEvent);
-            console.log("Connected to poloniex");
-        };
-        exchangeConn.open();
-    });
     console.log("Connected to database");
+    db.createCollection('marketdata', function (error, collection) {
+        var uri = url.parse("https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-pivx", true);
+        setInterval(function () {
+            var req = https.request({
+                host: uri.host,
+                path: uri.path,
+            }, function (res) {
+                res.setEncoding('utf8');
+                res.on('data', function (data) {
+                    var doc = JSON.parse(data.toString());
+                    collection.insert(doc);
+                });
+            });
+            req.end();
+            req.on('error', function (err) {
+                console.error(err);
+            });
+        }, 5 * 1000);
+    });
 });
--- datagrabber.ts
+++ datagrabber.ts
@@ -1,80 +1,37 @@
-import { Connection, Session } from 'autobahn';
 import { MongoClient, MongoError, MongoCallback, Db, Collection } from 'mongodb';
-
-
-var test = [ { type: 'orderBookModify',
-    data: { type: 'ask', rate: '0.02315653', amount: '0.03078201' } } ];
-
-var test2 = [ { type: 'orderBookRemove',
-    data: { type: 'bid', rate: '0.01559075' } },
-  { type: 'orderBookModify',
-    data: { type: 'bid', rate: '0.01559080', amount: '0.05169741' } } ];
-
+import * as url from 'url';
+import * as https from 'https';
+import { IncomingMessage } from 'http';
 
 var dbConn = MongoClient.connect("mongodb://localhost:27017/db", function (error: MongoError, db: Db) {
     if (error) {
-        console.log("Error object truthy");
-        return console.dir(error);
+        console.error(error);
+        return;
     }
 
-    var block:any = {};
-    var blocks:any = [];
-    var lastTrade:any = {};
-    var lastSeq = 0;
-
-    db.createCollection('marketdata', function (error: MongoError, collection: Collection) {
-        var exchangeConn = new Connection({ url: 'wss://api.poloniex.com', realm: 'realm1' });
-
-        exchangeConn.onopen = function (session:Session) {
-
-            function marketEvent(args: any[], kwargs:any) {
-
-                if(args.length > 0) {
-                    block.seq = kwargs.seq;
-                    block.marketMovments = args;
-                    
-                    blocks.push(block);
-                }
-            }
-
-            session.subscribe('BTC_XMR', marketEvent);
-            console.log("Connected to poloniex");
-
-            setInterval(function() {
-
-                //if last seq not null, filter blocks by it
-
-                blocks.sort(function(one, two) {
-                    return one.seq - two.seq;
-                });
-
-                var marketdata = new Array(blocks.length);
-
-                //scan each seq for best ask & bid
-                //using last trade, store
-                //if hit trade, update last trade
-
-                //everything in a block is considered to have happened at the same time, so we basically just do min and max on it
-                blocks.forEach(b => {
-            
-                    //var bid = findBestBid(b); //highest bid
-                    //var ask = findBestAsk(b); //lowest ask
-                    //var trade = findBestTrade(b); //highest trade
-                    //store(bid, ask lastTrade);
-
-                    //lastTrade = trade;
-
-                });
-
-                //lastSeq = maxSeq(blocks);
-
-            }, 5*60*1000);
-
-
-        };
-
-        exchangeConn.open();
-    });
-
     console.log("Connected to database");
+    db.createCollection('marketdata', function (error: MongoError, collection: Collection) {
+        var uri = url.parse("https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-pivx", true);
+
+        setInterval(function () {
+            var req = https.request({
+                host: uri.host,
+                path: uri.path,
+            }, function (res: IncomingMessage) {
+                res.setEncoding('utf8');
+                res.on('data', function (data) {
+                    var doc = JSON.parse(data.toString());
+                    collection.insert(doc);
+                })
+            });
+
+            req.end();
+
+            req.on('error', function (err: Error) {
+                console.error(err);
+            });
+
+        }, 5 * 1000);
+
+    });
 });
